5d06d197086c4f71f93b58413b7407b24fdfd20a
Jobin Murickan <jobin.murickan@gmail.com>
2016-09-25 16:48:44 -0500

    making progress

--- quiz.py
+++ quiz.py
@@ -1,44 +1,52 @@
 from __future__ import print_function
-from Tkinter import *
+from tkinter import *
 from PIL import Image, ImageTk
 
 
-class App:
+class App(Frame):
 
-    frame = Frame()
-
-    def __init__(self, master):
+    def __init__(self, parent):
 
         f = open("results.txt", "w")
-
+        Frame.__init__(self, parent, background="white")
         
-        frame = Frame(master)
-        frame.pack()
+        self.parent = parent
 
-        self.start = Button(frame, text="Start Quiz", command=self.starter)
+        self.initUI()
+
+    def initUI(self):
+
+        self.parent.title("UT NROTC Knowledge Game")
+        self.pack(fill=BOTH, expand = 1)
+
+        self.start = Button(self, text="Start Quiz", command=self.starter)
         self.start.pack()
-        self.lead = Button(frame, text="Leaderboard", command=self.leader)
+
+        self.lead = Button(self, text="Leaderboard", command=self.leader)
         self.lead.pack()
-
-
+ 
     def starter(self):
+
+        self.start.pack_forget()
+        self.lead.pack_forget()
 
         count = 0
 
         image = Image.open("images/c_2")
         photo = ImageTk.PhotoImage(image)
 
-        w = Label(frame, image=photo)
+        w = Label(self, image=photo)
         w.photo = photo
         w.pack()
 
         v = IntVar()
 
-        self.option = Radiobutton(frame, text="Cruiser", variable=v, value=1).pack(anchor=W)
-        self.option2 = Radiobutton(frame, text="Destroyer", variable=v, value=2).pack(anchor=W)
+        self.option = Radiobutton(self, text="Cruiser", variable=v, value=1).pack(anchor=W)
+        self.option2 = Radiobutton(self, text="Destroyer", variable=v, value=2).pack(anchor=W)
 
-        self.button = Button(frame, text="SUBMIT", command=self.submit)
+        self.button = Button(self, text="SUBMIT", command=self.submit)
         self.button.pack(side=LEFT)
+
 
     def submit(self):
         print ("submit")
